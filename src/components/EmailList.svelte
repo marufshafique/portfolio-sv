0b0e6c6c8e657f20fe712eda17607143c0cd1b2e
shm-wtag <maruf.shafique@welldev.io>
2025-05-27 01:24:28 +0600

    lint fix

--- src/components/EmailList.svelte
+++ src/components/EmailList.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Button from './Button.svelte';
-import Container from './Container.svelte';
+	import Container from './Container.svelte';
 	import SectionHeader from './SectionHeader.svelte';
 </script>
 
@@ -9,12 +9,12 @@ import Container from './Container.svelte';
 		<SectionHeader title="Email lists" />
 		<h1 class="text-7xl font-medium">Get design insights and articles straight to your inbox</h1>
 
-		<div class=mt-16>
+		<div class="mt-16">
 			<div class="flex gap-8">
-				<input class="w-1/3 p-3 rounded" type="text" />
+				<input class="w-1/3 rounded p-3" type="text" />
 				<Button>Subscribe</Button>
 			</div>
-			<p class="text-gray-500 mt-2">No spam, unsubscribe at any time.</p>
+			<p class="mt-2 text-gray-500">No spam, unsubscribe at any time.</p>
 		</div>
 	</Container>
 </section>
